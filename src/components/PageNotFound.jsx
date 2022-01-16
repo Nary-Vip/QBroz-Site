3f445915efeb6cbf4915948cb4a27801086cab74
Nary-Vip <nary2vip@gmail.com>
2022-01-16 12:34:10 +0530

    Qbroz: Added Page 404 Dark theme support

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeContext } from '../context/Context';
 
 const PageNotFound = () => {
+    const { theme } = useContext(ThemeContext);
+    let style;
+
+    if (theme === "dark") {
+        style = { background: "linear-gradient(168deg, rgba(255,255,255,1) 0%, rgba(0,0,0,1) 0%, rgba(34,34,34,0.8970938717283788) 69%)", color: "#DCDCDC" };
+    }
+    else if (theme === "light") {
+        style = { color: "black" };
+    }
+
     return (
-        <div className="h-screen pnf-bg">
+        <div style={style} className="h-screen pnf-bg">
             <div className="flex flex-col justify-between my-20 h-3/5">
                 <div className="my-10 text-center pnf-cont">
                     <h2 className="p-10 text-white ease-in-out rounded-lg font-balo">404 Uh ohh!</h2>
